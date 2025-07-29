6fc4017549f200d0d665468035cba9fd0b0819c2
Tomas Spirito <tomasspirito10@gmail.com>
2025-07-29 15:22:34 -0300

    Revert "first try deploy"
    This reverts commit 84f32d1e76947fd2deda7d8a1a58f2b0901c392d.

--- hooks/hooks.ts
+++ hooks/hooks.ts
@@ -2,9 +2,7 @@ import { useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { router } from 'expo-router';
 import { Alert } from 'react-native';
-//const url = 'https://turnitos-production.up.railway.app';
-const url = 'http://localhost:3000';
-
+const url = 'https://turnitos-production.up.railway.app';
 
 export const useCreatedRecipes = () => {
   const [recipes, setRecipes] = useState([]);
